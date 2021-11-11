ec79172bd086ac3b8b23a1953994eabecac43987
ivmanikhin <ivmanikhin@gmail.com>
2021-11-11 20:13:11 +0300

    Now it works

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas as pd
-student_data_frame = pd.DataFrame(student_dict)
-# print(student_data_frame)
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
+# Create a dictionary in this format:
 
 alphabet = pd.read_csv('nato_phonetic_alphabet.csv')
-dict_list = alphabet.to_dict('records')
-nato_dict = {item['letter']: item['code'] for item in dict_list}
-nato_dict = {item['letter']: item['code'] for item in alphabet.to_dict('records')}
+dict_list = alphabet.to_dict('split')['data']
+print(dict_list)
+nato_dict = {item[0]: item[1] for item in dict_list}
 print(nato_dict)
-{"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# Create a list of the phonetic code words from a word that the user inputs.
 
+user_name = list(input("What's your name?").upper())
+print(user_name)
+output = [nato_dict[letter] for letter in user_name]
+print(output)
